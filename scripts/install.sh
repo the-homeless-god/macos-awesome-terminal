d8eb026259bd340fe8d2fe6f52af2196e17fb3c7
Marat Zimnurov <zimtir@mail.ru>
2021-03-19 21:37:59 +0300

    feat: add brew script for installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,21 @@
 echo "Start"
 
-echo "0. Creating of config for ZSH"
+echo "0. Installation of Brew"
+sh ./install_brew.sh
+
+echo "1. Creating of config for ZSH"
 sh ./install_zhsrc.sh
 
-echo "1. Creating of config for Tmux"
+echo "2. Creating of config for Tmux"
 sh ./install_tmux.sh
 
-echo "2. Creating of config for Alacritty"
+echo "3. Creating of config for Alacritty"
 sh ./install_alacritty.sh
 
-echo "3. Installation of Vim"
+echo "4. Installation of Vim"
 sh ./install_vim.sh
 
-echo "4. Installation of neofetch"
+echo "5. Installation of neofetch"
 sh ./install_neofetch.sh
 
 echo "Finish"
